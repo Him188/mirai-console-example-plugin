30b1fb50c60a93097c90d3880dec7405d522f7e6
Him188 <Him188@mamoe.net>
2020-12-01 09:46:16 +0800

    publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,15 +2,19 @@ plugins {
     val kotlinVersion = "1.4.20"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
-    id("net.mamoe.mirai-console") version "1.0.1"
+    id("net.mamoe.mirai-console") version "1.1.0-dev-36"
 }
 
 mirai {
-
+    publishing {
+        repo = "mirai"
+        packageName = "mirai-console-example-plugin"
+        override = true
+    }
 }
 
 group = "org.example"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 repositories {
     mavenLocal()
